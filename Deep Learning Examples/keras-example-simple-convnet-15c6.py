ebff9a5ee75bd4f20eae3a3857e896f274e46130
the-laughing-monkey <dan@meuploads.com>
2017-02-26 17:41:04 -0800

    New architecture added

--- Deep Learning Examples/keras-example-simple-convnet-15c6.py
+++ Deep Learning Examples/keras-example-simple-convnet-15c6.py
@@ -1,24 +1,25 @@
-'''Trains a simple convnet on the CIFAR10 dataset.
-Modified from Keras examples
+'''Trains a simple convnet on the MNIST dataset.
+Gets to 99.25% test accuracy after 12 epochs
+(there is still a lot of margin for parameter tuning).
+16 seconds per epoch on a GRID K520 GPU.
 '''
 
 from __future__ import print_function
 import numpy as np
-
+np.random.seed(1337)  # for reproducibility
 
 from keras.datasets import cifar10
 from keras.callbacks import TensorBoard
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Convolution2D, MaxPooling2D
+from keras.layers import Convolution2D, MaxPooling2D, AveragePooling2D, UpSampling2D, AtrousConvolution2D
+from keras.layers.advanced_activations import LeakyReLU, PReLU
 from keras.utils import np_utils
 from keras import backend as K
 
-np.random.seed(1337)  # Seed random values to ensure reproducability of results
-
-batch_size = 128
+batch_size = 256
 nb_classes = 10
-nb_epoch = 35
+nb_epoch = 75
 
 # input image dimensions
 img_rows, img_cols = 32, 32
@@ -58,27 +59,39 @@ model = Sequential()
 model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1],
                         border_mode='valid',
                         input_shape=input_shape))
-model.add(Activation('relu'))
+model.add(LeakyReLU(alpha=.003))
+#model.add(UpSampling2D(size=pool_size))
+model.add(AtrousConvolution2D(64, 3, 3, border_mode='same'))
+model.add(LeakyReLU(alpha=.003))
 model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1]))
-model.add(Activation('relu'))
+model.add(LeakyReLU(alpha=.003))
+model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1]))
+model.add(MaxPooling2D(pool_size=pool_size))
+model.add(Dropout(0.35))
+
+model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1]))
+model.add(LeakyReLU(alpha=.003))
+#model.add(UpSampling2D(size=pool_size))
+model.add(AtrousConvolution2D(64, 3, 3, border_mode='same'))
+model.add(LeakyReLU(alpha=.003))
+model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1]))
+model.add(LeakyReLU(alpha=.003))
 model.add(MaxPooling2D(pool_size=pool_size))
 model.add(Dropout(0.25))
 
-model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1]))
-model.add(Activation('relu'))
-model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1]))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=pool_size))
-model.add(Dropout(0.25))
 
 model.add(Flatten())
 model.add(Dense(256))
 model.add(Activation('relu'))
-model.add(Dropout(0.5))
+model.add(Dropout(0.35))
+model.add(Dense(256))
+model.add(Activation('relu'))
+model.add(Dropout(0.25))
 model.add(Dense(nb_classes))
 model.add(Activation('softmax'))
 
 model.compile(loss='categorical_crossentropy',
+#              optimizer='adadelta',
               optimizer='adam',
               metrics=['accuracy'])
 
@@ -87,7 +100,6 @@ tb = TensorBoard(log_dir='./logs')
 
 model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
           verbose=1, validation_data=(X_test, Y_test), callbacks=[tb])
-
 
 score = model.evaluate(X_test, Y_test, verbose=0)
 
